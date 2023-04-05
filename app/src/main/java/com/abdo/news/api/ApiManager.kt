2005b3f6ef44a0b6dc74c4b97b82cd4a24ce2be0
boody <boodayman936@gmail.com>
2023-04-05 21:58:25 +0200

    create API for categories create ApiManager.kt finish categories API for CategoriesFragment create a progressBar

---
+++ app/src/main/java/com/abdo/news/api/ApiManager.kt
@@ -0,0 +1,43 @@
+package com.abdo.news.api
+
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+@Suppress("UNREACHABLE_CODE")
+class ApiManager {
+    companion object {
+        private val BASE_URL = " https://newsapi.org/"
+        private var retrofit: Retrofit? = null
+
+        private fun getInstance(): Retrofit {
+            if (retrofit == null) {
+
+                val logging = HttpLoggingInterceptor(
+                    object : HttpLoggingInterceptor.Logger {
+                        override fun log(message: String) {
+                            Log.e("api", message)
+                        }
+                    })
+                logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
+                val client: OkHttpClient = OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build()
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit!!
+
+        }
+
+        fun getApis(): Services {
+            return getInstance().create(Services::class.java)
+        }
+    }
+}
